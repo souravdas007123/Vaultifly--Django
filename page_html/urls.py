e3b9fe916bfa3d19aa4a55a1f8f148ee63d940c9
Sourav Das <souravdas007123@gmail.com>
2025-10-27 17:42:55 +0530

    pages create and block added

--- page_html/urls.py
+++ page_html/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from page_html.views import home,asset,project
+from page_html.views import home,asset,project,projectoverview,projectworkspace,finance,inventory
 urlpatterns = [
     path('', home,name='home'),
     path('asset/', asset,name='asset'),
     path('project/', project,name='project'),
+    path('projectoverview/', projectoverview,name='projectoverview'),
+    path('projectworkspace/', projectworkspace,name='projectworkspace'),
+    path('finance/', finance,name='finance'),
+    path('inventory/', inventory,name='inventory'),
+
+
 
 ]
--- page_html/views.py
+++ page_html/views.py
@@ -9,3 +9,15 @@ def asset(request):
 
 def project(request):
     return render(request,'page_html/project.html')
+
+def projectoverview(request):
+    return render(request,'page_html/project_overview.html')
+
+def projectworkspace(request):
+    return render(request,'page_html/project_workspace.html')
+
+def finance(request):
+    return render(request,'page_html/finance.html')
+
+def inventory(request):
+    return render(request,'page_html/inventory.html')
